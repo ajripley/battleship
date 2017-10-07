cd86bae09ec50b5f62250b13e3b1162a392dc18c
ajripley <alaina.j.ripley@gmail.com>
2017-10-07 12:19:38 -0700

    Add React, ESLint, and Husky; add precommit hook for ESLint with Husky

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount'),
+  );
+});
